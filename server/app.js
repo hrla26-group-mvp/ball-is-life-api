ad3f70859a456e7e365eff19c607082739676183
Martin Han <martinhan@Martins-MacBook-Pro-2.local>
2019-01-30 16:37:18 -0800

    [prog] Edit route to server player data

--- server/app.js
+++ server/app.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
+const cors = require('cors');
 const router = require('./router');
 
 const app = express();
 
+app.use(cors());
 app.use(morgan('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
